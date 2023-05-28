0ac98a0651e397b82262ad9d7ed4e5beced11fee
Ruben <rubenvegajusto@gmail.com>
2023-05-28 12:33:33 +0100

    I calculate the average using for cycles

--- Statistics/statistics.js
+++ Statistics/statistics.js
@@ -2,11 +2,11 @@ function CalculateAverage(list) {
   //sum all the array elements / elements quantity
 
   let listSum = 0;
-  for (let i = 0; i < list.lenght; i++) {
-    listSum = sumList + list[i];
+  for (let i = 0; i < list.length; i++) {
+    listSum = listSum + list[i];
   }
 
-  const average = listSum / list.lenght;
+  const average = listSum / list.length;
   console.log(average);
   return average;
 }
