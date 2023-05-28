2e8491bec781dbac3714cc907545d5b4de906616
Ruben <rubenvegajusto@gmail.com>
2023-05-28 15:08:58 +0100

    Order lists algorithm using the sort JavaScript method

--- Statistics/statistics.js
+++ Statistics/statistics.js
@@ -1,30 +1,9 @@
 function isEven(list) {
   return !(list.length % 2);
 }
+
 function isOdd(list) {
   return list.length % 2;
-}
-
-function calculateMedian(list) {
-  const listIsEven = isEven(list);
-
-  if (listIsEven) {
-    const indexFirstHalfListEven = list.length / 2 - 1;
-    const indexSecondHalfListEven = list.length / 2;
-
-    const listHalfs = [];
-    listHalfs.push(list[indexFirstHalfListEven]);
-    listHalfs.push(list[indexSecondHalfListEven]);
-
-    const medianListEven = calculateAverage(listHalfs);
-    return medianListEven;
-  } else {
-    const indexHalfListOdd = Math.floor(list.length / 2);
-    const medianListOdd = list[indexHalfListEven];
-    console.log(indexHalfListEven);
-    console.log(medianListEven);
-    return medianListEven;
-  }
 }
 
 function calculateAverage(list) {
@@ -34,6 +13,39 @@ function calculateAverage(list) {
 
   const sumList = list.reduce(sumAllElements);
   const average = sumList / list.length;
-  // console.log(promedio);
   return average;
 }
+
+function orderList(disorderedList) {
+  function orderListSort(accumulatedValue, newValue) {
+    return accumulatedValue - newValue;
+  }
+
+  const list = disorderedList.sort(orderListSort);
+  return list;
+}
+
+function calculateMedian(list) {
+  const orderedList = orderList(list);
+  const listIsEven = isEven(orderedList);
+
+  if (listIsEven) {
+    const indexFirstHalfListEven = orderedList.length / 2 - 1;
+    const indexSecondHalfListEven = orderedList.length / 2;
+
+    const listHalfs = [];
+    listHalfs.push(orderedList[indexFirstHalfListEven]);
+    listHalfs.push(orderedList[indexSecondHalfListEven]);
+
+    const medianListEven = calculateAverage(listHalfs);
+    return medianListEven;
+  } else {
+    const indexHalfListOdd = Math.floor(orderedList.length / 2);
+    const medianListOdd = orderedList[indexHalfListOdd];
+    return medianListOdd;
+  }
+}
+
+// Example usage:
+const myList = [5, 2, 8, 3, 9];
+console.log(calculateMedian(myList));
