48353d9bf9e0639288fc7d706768a29c1da2d265
Ruben <rubenvegajusto@gmail.com>
2023-05-28 12:50:09 +0100

    I did the average using arrow functions

--- Statistics/statistics.js
+++ Statistics/statistics.js
@@ -1,10 +1,13 @@
 function CalculateAverage(list) {
   //sum all the array elements / elements quantity
 
-  let listSum = 0;
-  for (let i = 0; i < list.length; i++) {
-    listSum = listSum + list[i];
-  }
+  // let listSum = 0;
+  // for (let i = 0; i < list.length; i++) {
+  //listSum = listSum + list[i];
+  // }
+
+  const elementsSum(accumulatedValue, newValue) => {};  
+  const listSum = list.reduce(elementSum);
 
   const average = listSum / list.length;
   console.log(average);
